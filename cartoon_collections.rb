eacf63aff1d524afef732b5924bf930ae9875b4b
Ruari Day <ruariday@gmail.com>
2020-03-10 14:30:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,6 +20,5 @@ end
 def find_the_cheese(cheeses)
   cheeses.find do |cheese|
     cheese == "cheddar" || cheese == "gouda" || cheese == "camembert" 
-  cheese_types = ["cheddar", "gouda", "camembert"]
  end 
 end
