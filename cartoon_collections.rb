9db90d0797dc2894cc2f88988f71115cdec61222
Ruari Day <ruariday@gmail.com>
2020-03-10 14:23:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,14 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  i = 0 
+  dwarves.each_with_index {|name, index| puts "#{index + 1}. #{name} "}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  new_planeteer_calls = []
+  planeteer_calls.map do |call|
+    new_planeteer_calls << "#{call.capitalize}!"
+  end 
+  new_planeteer_calls
 end
 
 def long_planeteer_calls# code an argument here
